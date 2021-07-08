17b922b3ea0ed88691f582a1f3ebd709bc10e8a8
marlloncsoares@gmail.com <marlloncsoares@gmail.com>
2021-07-08 20:47:05 -0300

    Refatoração de código

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: 'src/database/database.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     CatsModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
